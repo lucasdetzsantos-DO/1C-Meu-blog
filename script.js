56067145ccb268636153925074f532e050cde624
lucasdetzsantos-DO <lucas.detz.santos@escola.pr.gov.br>
2026-09-23 08:43:23 -0300

    identacao

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-   const botoes = document.querySelectorAll("button")
-    botoes.forEach(function (botao) {
-        let curtiu = false
-        botao.addEventListener("click", botaoClicado)
+const botoes = document.querySelectorAll("button")
+botoes.forEach(function (botao) {
+    let curtiu = false
+    botao.addEventListener("click", botaoClicado)
 
-        function botaoClicado() {
-            console.log("fui clicado")
-            let texto = botao.querySelector("span")
-            if (curtiu === false) {
+    function botaoClicado() {
+        console.log("fui clicado")
+        let texto = botao.querySelector("span")
+        if (curtiu === false) {
                 texto.textContent++;
                 curtiu = true
-            } else {
-                texto.textContent--;
-                curtiu = false
-            }
+        } else {
+            texto.textContent--;
+            curtiu = false
         }
-    })
+    }
+})
